1229e584c74369eac28a3b1ffacb76672c794f68
Nicolas Duhamel <nicolas@jombi.fr>
2011-11-19 18:06:58 +0100

    introduce episode state and add per serie config

--- canape/xmldb.py
+++ canape/xmldb.py
@@ -30,13 +30,18 @@ from canape.utils import synchronized
 LOGGER = logging.getLogger(__name__)
 LOCK = Lock()
 
+WAITING = 1
+SUBTITLE_DOWNLOADED = 2
+VIDEO_DOWNLOADING = 4
+VIDEO_DOWNLOADED = 8
+
 class Canapedb(object):
     
     def __init__(self, xmlfile):
         self.xmlfile = xmlfile
     
     @synchronized(LOCK)
-    def add_serie(self, name, lastest_snum, lastest_enum):
+    def add_serie(self, name, lastest_snum, lastest_enum, state=None, quality=None, subtitle=None):
         """ Current approach:
         1/ Iter all db and copy it to a tempfile
         2/ Add node
@@ -47,15 +52,21 @@ class Canapedb(object):
         Cons:
          - Many file access
         """
+        state = state or WAITING
+        extra = {}
+        if quality is not None:
+            extra['quality'] = quality
+        if subtitle is not None:
+            extra['subtitle'] = subtitle
         tmp_file  = tempfile.NamedTemporaryFile(delete=False)
         tmp_file.write('<series>\n')
         def do(elem):
             tmp_file.write( etree.tostring(elem,pretty_print=False))
         # Copy original
-        self._fast_iter(do)
+        self._fast_iter_serie(do)
         # Add serie:
-        serie = etree.Element("serie", name=name)
-        etree.SubElement(serie, "episode", snum=str(lastest_snum), enum=str(lastest_enum))
+        serie = etree.Element("serie", name=name, **extra)
+        etree.SubElement(serie, "episode", snum=str(lastest_snum), enum=str(lastest_enum), state=str(state))
         tmp_file.write(etree.tostring(serie,pretty_print=True))
         LOGGER.debug('Add %s to season %s episode %s' % (name, lastest_snum, lastest_enum))
         #End
@@ -63,22 +74,83 @@ class Canapedb(object):
         tmp_file.close()
         shutil.move(tmp_file.name, self.xmlfile)
         
+    @synchronized(LOCK)    
+    def add_episode(self, sname, snum, enum, state=None):
+        state = state or WAITING
+        tmp_file  = tempfile.NamedTemporaryFile(delete=False)
+        tmp_file.write('<series>\n')
+        def do(elem):
+            if elem.attrib['name'] == sname:
+                etree.SubElement(elem, "episode", snum=str(snum), enum=str(enum),state=str(state))
+            tmp_file.write( etree.tostring(elem,pretty_print=False))
+        # Copy original
+        self._fast_iter_serie(do)
+        if self.tmpfound:
+            tmp_file.write('</series>')
+            tmp_file.close()
+            shutil.move(tmp_file.name, self.xmlfile)
+        else:
+            LOGGER.error("Can't update %s because it don't exist in db" % name)
+            tmp_file.close()
+            os.remove(tmp_file.name)
+            
+    @synchronized(LOCK)    
+    def remove_episode(self, sname, snum, enum):
+        self.tmpfound=False
+        tmp_file  = tempfile.NamedTemporaryFile(delete=False)
+        tmp_file.write('<series>\n')
+        def do(elem):
+            if elem.attrib['name'] == sname:
+                serie = etree.Element("serie", name=sname)
+                def return_ep(elem):
+                    ep_snum = int(elem.attrib['snum'])
+                    ep_enum = int(elem.attrib['enum'])
+                    ep_state = int(elem.attrib['state'])
+                    if not ep_snum == snum or not ep_enum == enum :
+                        etree.SubElement(serie, "episode", snum=str(ep_snum), enum=str(ep_enum),state=str(ep_state))
+                    else:
+                        self.tmpfound=True
+                self._fast_iter_episode(elem,return_ep)
+                tmp_file.write( etree.tostring(serie,pretty_print=False))
+            else:
+                tmp_file.write( etree.tostring(elem,pretty_print=False))
+        # Copy original
+        self._fast_iter_serie(do)
+        if self.tmpfound:
+            tmp_file.write('</series>')
+            tmp_file.close()
+            shutil.move(tmp_file.name, self.xmlfile)
+        else:
+            LOGGER.error("Can't delete because it don't exist in db" )
+            tmp_file.close()
+            os.remove(tmp_file.name)
+        
+        
     @synchronized(LOCK)
-    def update_serie(self, name, new_snum, new_enum):
+    def update_episode(self, name, snum, enum, state):
+        """ Update serie's episode state """
+        if state == WAITING:
+            return
         self.tmpfound=False
         tmp_file  = tempfile.NamedTemporaryFile(delete=False)
         tmp_file.write('<series>\n')
         def do(elem):
             if elem.attrib['name'] == name:
-                self.tmpfound=True
-                LOGGER.debug('Update %s to season %s episode %s' % (name, new_snum, new_enum))
                 serie = etree.Element("serie", name=name)
-                etree.SubElement(serie, "episode", snum=str(new_snum), enum=str(new_enum))
+                def return_ep(elem):
+                    ep_snum = int(elem.attrib['snum'])
+                    ep_enum = int(elem.attrib['enum'])
+                    ep_state = int(elem.attrib['state'])
+                    if ep_snum == snum and ep_enum == enum:
+                        ep_state = ep_state | state
+                        self.tmpfound=True
+                    etree.SubElement(serie, "episode", snum=str(snum), enum=str(enum),state=str(ep_state))
+                self._fast_iter_episode(elem,return_ep)
                 tmp_file.write( etree.tostring(serie,pretty_print=False))
             else:
                 tmp_file.write( etree.tostring(elem,pretty_print=False))
         # Copy original
-        self._fast_iter(do)
+        self._fast_iter_serie(do)
         if self.tmpfound:
             tmp_file.write('</series>')
             tmp_file.close()
@@ -110,7 +182,7 @@ class Canapedb(object):
                 self.tmpfound = True
                 LOGGER.debug('Remove %s' % name)
         # Copy original
-        self._fast_iter(do)
+        self._fast_iter_serie(do)
         if self.tmpfound:
             tmp_file.write('</series>')
             tmp_file.close()
@@ -125,18 +197,35 @@ class Canapedb(object):
         context = etree.iterparse(self.xmlfile, events=('end',), tag='serie')
         for event, elem in context:
             name = elem.attrib['name']
-            snum = int(elem[0].attrib['snum'])
-            enum = int(elem[0].attrib['enum'])
+            quality = elem.attrib.get('quality', None)
+            subtitle = elem.attrib.get('subtitle', None)
+            ep=[]
+            def return_ep(elem):
+                snum = int(elem.attrib['snum'])
+                enum = int(elem.attrib['enum'])
+                state = int(elem.attrib['state'])
+                ep.append({'snum':snum,'enum':enum,'state':state})
+            self._fast_iter_episode(elem,return_ep)
             #Clear memory
             elem.clear()
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
             # Create a generator
-            yield {'name':name, 'snum':snum, 'enum':enum}
+            yield {'name':name, 'episodes':ep, 'quality':quality, 'subtitle':subtitle}
         del context
     
-    def _fast_iter(self, function):
+    def _fast_iter_serie(self, function):
         context = etree.iterparse(self.xmlfile, events=('end',), tag='serie')
+        for event, elem in context:
+            function(elem)
+            #Clear memory
+            elem.clear()
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+        del context
+        
+    def _fast_iter_episode(self, sele, function):
+        context = etree.iterwalk(sele, events=('end',), tag='episode')
         for event, elem in context:
             function(elem)
             #Clear memory
@@ -146,18 +235,33 @@ class Canapedb(object):
         del context
 
 if __name__ == '__main__':
-    db = Canapedb('testdb.xml')
+    db = Canapedb('textdb.xml')
     print "All series:"
     for s in db.get_series():
         print s
     print '-----------------------------------------'
-    print 'Add Dr House and print:'
-    db.add_serie('Dr House', 1,1)
+    print 'Add Dr House 720p and print:'
+    db.add_serie('Dr House', 1,1,quality='720p')
     for s in db.get_series():
         print s
     print '------------------------------------------'
-    print 'Update Dr House to season 3, episode 4 and print'
-    db.update_serie('Dr House', 3, 4)
+    print 'Update Dr House to season 1, episode 1 to VIDEO_DOWNLOADING and print'
+    db.update_episode('Dr House', 1, 1, state=VIDEO_DOWNLOADING)
+    for s in db.get_series():
+        print s
+    print '------------------------------------------'
+    print 'Update Dr House to season 1, episode 1 to SUBTITLE_DOWNLOADED and print'
+    db.update_episode('Dr House', 1, 1, state=SUBTITLE_DOWNLOADED)
+    for s in db.get_series():
+        print s
+    print '------------------------------------------'
+    print 'Add Dr House season 1, episode 2 and print'
+    db.add_episode('Dr House', 1, 2)
+    for s in db.get_series():
+        print s
+    print '------------------------------------------'
+    print 'Remove Dr House season 1, episode 1 and print'
+    db.remove_episode('Dr House', 1, 1)
     for s in db.get_series():
         print s
     print '------------------------------------------'
